a56927dbf0f862fa339ea96b0b1c581e6d295b4e
rsuriano <ramirosuriano@gmail.com>
2019-01-09 04:57:38 -0300

    [Tags] Comienzo de la clase Tag
    Faltan los procedimientos de almacenado (hacer conexion primero)

---
+++ ConnectionDispensario/ConnectionDispensario/Modelos/Tag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionDispensario.Modelos
+{
+    [Serializable]
+    class Tag
+    {
+        private int id;
+        private string nombre;
+        private string icono;
+
+        public string NOMBRE { get { return nombre; } set { nombre = value; } }
+        public string ICONO { get { return icono; } set { icono = value; } }
+
+        public Tag()
+        {
+
+        }
+
+        public Tag(string p_nombre, string p_icono)
+        {
+            nombre = p_nombre;
+            icono = p_icono;
+        }
+
+        public Tag(DataRow DR)
+        {
+            id = int.Parse(DR["Id"].ToString());
+            nombre = DR["Nombre"].ToString();
+            icono = DR["Icono"].ToString();
+        }
+
+    }
+}
