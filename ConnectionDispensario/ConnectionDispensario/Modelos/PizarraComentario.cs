147e6ed7f0848c998e89c5a97a9e9b25827a8d12
Ramiro <ramirosuriano@gmail.com>
2019-02-21 18:01:29 -0300

    [Pizarra] Finalizada la capa de conexion y modelos

---
+++ ConnectionDispensario/ConnectionDispensario/Modelos/PizarraComentario.cs
@@ -0,0 +1,87 @@
+﻿using ConnectionDispensario.Conexiones;
+using ConnectionDispensario.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionDispensario.Modelos
+{
+    [Serializable]
+    public class PizarraComentario
+    {
+        int id;
+        int idpost;
+        string comentario;
+        int idprofesional;
+        DateTime fechacreacion;
+
+        public int Id { get => id; set => id = value; }
+        public int Idpost { get => idpost; set => idpost = value; }
+        public string Comentario { get => comentario; set => comentario = value; }
+        public int Idprofesional { get => idprofesional; set => idprofesional = value; }
+        public DateTime Fechacreacion { get => fechacreacion; set => fechacreacion = value; }
+
+        Con_Pizarra CON = new Con_Pizarra();
+
+        public PizarraComentario()
+        {
+
+        }
+
+        public PizarraComentario(int id, int idpost, string comentario, int idprofesional, DateTime fechacreacion)
+        {
+            Id = id;
+            Idpost = idpost;
+            Comentario = comentario;
+            Idprofesional = idprofesional;
+            Fechacreacion = fechacreacion;
+        }
+
+        public PizarraComentario(DataRow DR)
+        {
+            Id = int.Parse(DR["Id"].ToString());
+            Idpost = int.Parse(DR["IdPost"].ToString());
+            Comentario = DR["Comentario"].ToString();
+            Idprofesional = int.Parse(DR["IdProfesional"].ToString());
+            Fechacreacion = DateTime.Parse(DR["FechaCreacion"].ToString());
+        }
+
+        public int InsertarComentario()
+        {
+            DataTable DT = CON.Insertar_ComentarioPizarra(Idpost, Comentario, Idprofesional, Conversiones.SQL_To_FullString_DateTime(Fechacreacion));
+            
+            if (DT != null)
+            {
+                int lastcommentId = int.Parse(DT.Rows[0][0].ToString());
+                return lastcommentId;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static List<PizarraComentario> ComentariosPorPost(int idpost)
+        {
+            Con_Pizarra c = new Con_Pizarra();
+            DataTable DT = c.Select_ComentariosByIdPost(idpost);
+            
+            if (DT != null)
+            {
+                List<PizarraComentario> aux = new List<PizarraComentario>();
+                foreach (DataRow comentario in DT.Rows)
+                {
+                    aux.Add(new PizarraComentario(comentario));
+                }
+                return aux;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- ConnectionDispensario/ConnectionDispensario/Modelos/PizarraPost.cs
+++ ConnectionDispensario/ConnectionDispensario/Modelos/PizarraPost.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using ConnectionDispensario.Conexiones;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConnectionDispensario.Utils;
 
 namespace ConnectionDispensario.Modelos
 {
@@ -16,6 +18,7 @@ namespace ConnectionDispensario.Modelos
         private DateTime fechacreacion;
         private int idpaciente;
         private string estado;
+        List<PizarraComentario> comentarios;
 
         public int Id { get => id; set => id = value; }
         public string Titulo { get => titulo; set => titulo = value; }
@@ -23,6 +26,7 @@ namespace ConnectionDispensario.Modelos
         public DateTime Fechacreacion { get => fechacreacion; set => fechacreacion = value; }
         public int Idpaciente { get => idpaciente; set => idpaciente = value; }
         public string Estado { get => estado; set => estado = value; }
+        internal List<PizarraComentario> Comentarios { get => comentarios; set => comentarios = value; }
 
         private Conexiones.Con_Pizarra CON = new Conexiones.Con_Pizarra();
 
@@ -39,6 +43,7 @@ namespace ConnectionDispensario.Modelos
             Fechacreacion = fechacreacion;
             Idpaciente = idpaciente;
             Estado = estado;
+            Comentarios = PizarraComentario.ComentariosPorPost(Id);
         }
 
         public PizarraPost(DataRow DR)
@@ -49,11 +54,54 @@ namespace ConnectionDispensario.Modelos
             Fechacreacion = DateTime.Parse(DR["FechaCreacion"].ToString());
             Idpaciente = int.Parse(DR["IdPaciente"].ToString());
             Estado = DR["Estado"].ToString();
+            Comentarios = PizarraComentario.ComentariosPorPost(Id);
         }
 
-        /*public static InsertarPost(string titulo, int idprofesional, DateTime fechacreacion, int idpaciente, string estado)
+        public int InsertarPost()
         {
+            DataTable DT = CON.Insert_Post(Titulo, Idprofesional, Conversiones.SQL_To_FullString_DateTime(Fechacreacion), Idpaciente, Estado);
 
-        }*/
+            if (DT != null)
+            {
+                int lastPostId = int.Parse(DT.Rows[0][0].ToString());
+                return lastPostId;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+
+        public static bool ActualizarEstado(int idpost, string estado)
+        {
+            Con_Pizarra c = new Con_Pizarra();
+            bool result = c.Update_PostStatus(idpost, estado);
+
+            return result;
+        }
+
+        public static List<PizarraPost> PostsPorPaciente(int idpaciente)
+        {
+            Con_Pizarra c = new Con_Pizarra();
+            DataTable DT = c.Select_PostsByIdPaciente(idpaciente);
+
+            if (DT != null)
+            {
+                List<PizarraPost> aux = new List<PizarraPost>();
+                foreach (DataRow post in DT.Rows)
+                {
+                    aux.Add(new PizarraPost(post));
+                }
+                return aux;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+            
     }
 }
